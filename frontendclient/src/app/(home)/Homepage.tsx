67d8f50fdf65cdc7ed2e449165743a02198822b9
Second <drewuncle863724lu@gmail.com>
2025-04-30 22:28:48 +0800

    search params error fixed

---
+++ frontendclient/src/app/(home)/Homepage.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import Navbar from "./_components/Navbar";
+import Order from "./_components/Order";
+import { Categories } from "./_components/Categories";
+import { Foods } from "./_components/Foods";
+
+const Homepage = () => {
+  return (
+    <div className="flex flex-col lg:flex-row h-screen">
+      <Navbar />
+
+      <div className="flex flex-col flex-grow overflow-y-auto bg-[#0F0F0F]">
+        <div className="sticky top-0 z-10">
+          <Categories />
+        </div>
+        <div className="flex-grow overflow-y-auto mt-4">
+          <Foods />
+        </div>
+      </div>
+
+      <div className="w-full lg:w-[30vw]">
+        <Order />
+      </div>
+    </div>
+  );
+};
+
+export default Homepage;
--- frontendclient/src/app/(home)/page.tsx
+++ frontendclient/src/app/(home)/page.tsx
@@ -1,31 +1,12 @@
-import Navbar from "./_components/Navbar";
-import Order from "./_components/Order";
-import { Categories } from "./_components/Categories";
-import { Foods } from "./_components/Foods";
+"use client";
+
 import { Suspense } from "react";
+import Homepage from "./Homepage"; 
 
-const Homepage = () => {
+export default function Page() {
   return (
-    <div className="flex flex-col lg:flex-row h-screen">
-    <Navbar />
-  
-    <div className="flex flex-col flex-grow overflow-y-auto bg-[#0F0F0F]">
-      <div className="sticky top-0 z-10">
-        <Categories />
-      </div>
-      <div className="flex-grow overflow-y-auto mt-4">
-        <Suspense fallback={<div>Loading foods...</div>}>
-          <Foods />
-        </Suspense>
-      </div>
-    </div>
-  
-    <div className="w-full lg:w-[30vw]">
-      <Order />
-    </div>
-  </div>
-  
+    <Suspense fallback={<div className="text-white text-center mt-10">Loading page...</div>}>
+      <Homepage />
+    </Suspense>
   );
-};
-
-export default Homepage;
+}
